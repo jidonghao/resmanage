e8ae0b67aae562c4a6bc13c5ae4a5961e7ffe245
jidonghao <ji534@outlook.com>
2023-01-20 15:11:53 +0800

    sql

---
+++ resmanage-sql/res_folder.sql
@@ -0,0 +1,7 @@
+INSERT INTO `res_folder` (`id`, `user_id`, `folder_name`, `update_time`, `add_time`, `deleted`) VALUES (1, 1, '测试', '2023-01-20 14:23:03', '2023-01-20 14:10:57', 0);
+INSERT INTO `res_folder` (`id`, `user_id`, `folder_name`, `update_time`, `add_time`, `deleted`) VALUES (2, 1, '测试2', '2023-01-20 14:23:05', '2023-01-20 14:20:25', 0);
+INSERT INTO `res_folder` (`id`, `user_id`, `folder_name`, `update_time`, `add_time`, `deleted`) VALUES (7, 1, '123', '2023-01-20 14:23:05', '2023-01-20 14:23:05', 0);
+INSERT INTO `res_folder` (`id`, `user_id`, `folder_name`, `update_time`, `add_time`, `deleted`) VALUES (8, 1, '测试2', '2023-01-20 14:34:03', '2023-01-20 14:34:03', 0);
+INSERT INTO `res_folder` (`id`, `user_id`, `folder_name`, `update_time`, `add_time`, `deleted`) VALUES (9, 1, '测试2', '2023-01-20 14:34:17', '2023-01-20 14:34:17', 0);
+INSERT INTO `res_folder` (`id`, `user_id`, `folder_name`, `update_time`, `add_time`, `deleted`) VALUES (10, 1, '测试2', '2023-01-20 14:35:20', '2023-01-20 14:35:20', 0);
+INSERT INTO `res_folder` (`id`, `user_id`, `folder_name`, `update_time`, `add_time`, `deleted`) VALUES (11, 1, '测试2', '2023-01-20 14:35:30', '2023-01-20 14:35:30', 0);
---
+++ resmanage-sql/user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (`id`, `nick_name`, `passwd`, `identity`, `phone_number`, `avatar`, `deleted`) VALUES (1, '用户123212', '12345678', 9, '16688171257', 'https://oss.dhxt.fun/avatar_rabbit.png', 0);
+INSERT INTO `user` (`id`, `nick_name`, `passwd`, `identity`, `phone_number`, `avatar`, `deleted`) VALUES (2, 'liuming', '12345678', 9, '15512345678', 'https://oss.dhxt.fun/avatar_rabbit.png', 0);
