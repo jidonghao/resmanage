e11e73383d68e2f99b58e1cabb38cc02a0e5f5ca
纪东豪 <ji534@outlook.com>
2023-02-07 17:40:33 +0800

    sql:修改user数据结构

--- resmanage-sql/resmanage.sql
+++ resmanage-sql/resmanage.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50739
+ Source Server Version : 50740 (5.7.40)
  Source Host           : localhost:3306
  Source Schema         : resmanage
 
  Target Server Type    : MySQL
- Target Server Version : 50739
+ Target Server Version : 50740 (5.7.40)
  File Encoding         : 65001
 
- Date: 30/01/2023 20:31:16
+ Date: 07/02/2023 17:40:15
 */
 
 SET NAMES utf8mb4;
@@ -35,7 +35,7 @@ CREATE TABLE `res_file`  (
   `query_time` datetime NULL DEFAULT NULL COMMENT '查看时间',
   `deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否被删除',
   PRIMARY KEY (`id`, `user_id`, `folder_id`, `label_id`, `type`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of res_file
@@ -81,7 +81,7 @@ CREATE TABLE `res_folder`  (
   `add_time` datetime NULL DEFAULT NULL COMMENT '添加时间',
   `deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否被删除',
   PRIMARY KEY (`id`, `user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of res_folder
@@ -106,7 +106,7 @@ CREATE TABLE `res_label`  (
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否被删除',
   PRIMARY KEY (`id`, `user_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of res_label
@@ -119,18 +119,22 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` int(64) NOT NULL AUTO_INCREMENT,
   `nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `passwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `passwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `identity` tinyint(1) NOT NULL DEFAULT 1 COMMENT '身份9超级管理员 具体代码限制',
-  `phone_number` varchar(22) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phone_number` varchar(22) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'https://oss.dhxt.fun/avatar_rabbit.png',
   `deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否被删除',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '用户创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '用户信息更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, '用户123212', '12345678', 9, '16688171257', 'https://oss.dhxt.fun/avatar_rabbit.png', 0);
-INSERT INTO `user` VALUES (2, 'liuming', '12345678', 9, '15512345678', 'https://oss.dhxt.fun/avatar_rabbit.png', 0);
+INSERT INTO `user` VALUES (1, '用户123212', '12345678', 9, '16688171257', 'https://oss.dhxt.fun/avatar_rabbit.png', 0, '2023-02-07 12:48:17', '2023-02-07 12:48:23');
+INSERT INTO `user` VALUES (2, 'liuming', '12345678', 9, '15512345678', 'https://oss.dhxt.fun/avatar_rabbit.png', 0, '2023-02-07 12:48:20', '2023-02-07 12:48:27');
+INSERT INTO `user` VALUES (3, '测试', NULL, 1, '13247362734', 'https://oss.dhxt.fun/avatar_rabbit.png', 0, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
+INSERT INTO `user` VALUES (4, '用户_7525979', NULL, 1, '16688171257', 'https://oss.dhxt.fun/avatar_rabbit.png', 0, '2023-02-07 13:02:53', '2023-02-07 13:02:53');
 
 SET FOREIGN_KEY_CHECKS = 1;
