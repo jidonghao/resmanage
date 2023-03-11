9f3c2b3cfb837747705849649862d508a434cea5
jidonghao <ji534@outlook.com>
2023-03-11 14:02:01 +0800

    单文件上传

--- resmanage-api/routes/index.js
+++ resmanage-api/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 import user from "./user.js"
 import file from "./file.js"
+import upload from "./upload.js"
 import {resJson} from "../index.js";
 
 router.use('/user',user);
 router.use('/file',file);
+router.use('/upload',upload);
 
 router.get('/getTime', (req, res, next) => {
     res.json(resJson(0, '成功', {time: new Date().getTime()}))
---
+++ resmanage-api/routes/upload.js
@@ -0,0 +1,67 @@
+import {resJson} from "../index.js";
+import express from "express";
+import fs from "fs";
+const router = express.Router();
+import {accessKeySecret, accessKeyId} from "../sms/sms-setting.js";
+import path from "path";
+import OSS from 'ali-oss'
+import multer from "multer";
+
+const client = new OSS({
+    region: 'oss-cn-beijing',
+    accessKeyId: accessKeyId,
+    accessKeySecret: accessKeySecret,
+    bucket: 'jdh-bucket',
+    secure: true,
+    endpoint: "oss.dhxt.fun",
+    cname: true
+})
+
+
+/**
+ * 上传文件到阿里云OSS
+ * @param fileName
+ * @param filePath
+ * @returns {Promise<unknown>}
+ */
+const putFile2Ali = (fileName, filePath) => new Promise((resolve, reject) => {
+    const __dirName = path.resolve(filePath)
+    client.put(`${fileName}`, path.normalize(__dirName)).then(res => {
+        resolve(res.url)
+    }).catch(err => {
+        console.error("上传到阿里云失败", err)
+        reject(err)
+    })
+})
+
+router.post('/upload', multer({dest: 'uploadFiles/'}).array('files'), async (req, res) => {
+    let files = req.files, fileList = [];
+    let {id = ''} = req.data
+    if (!id) {
+        res.json(resJson(601, '系统检测到异常行为，已被拦截。'))
+        return false
+    }
+    if (!files) {
+        res.json(resJson(601, '参数错误'))
+        return false
+    }
+
+    try{
+        await files.reduce(async (memo, item) => {
+            await memo
+            let newName = 'RES-' + Date.now().toString() + '-' + item.originalname
+            let newPath = "uploadFiles/" + newName
+            fs.renameSync("uploadFiles/" + item.filename, newPath);
+            let file = await putFile2Ali(newName, newPath)
+            fileList.push(file)
+        }, undefined);
+        res.json(resJson(0, '成功', {fileList}))
+    }catch (e) {
+            console.error("上传文件失败foreach：",e)
+            res.json(resJson(500, '系统内部错误'))
+            return false
+    }
+})
+
+
+export default router
