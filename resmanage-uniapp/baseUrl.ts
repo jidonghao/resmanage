b23d30d8f0ec42e02c6231acee77ba29143f7a02
jidonghao <ji534@outlook.com>
2023-06-09 00:45:40 +0800

    set pages and base url

--- resmanage-uniapp/baseUrl.ts
+++ resmanage-uniapp/baseUrl.ts
@@ -1,4 +1,4 @@
 // let BaseUrl: String = "http://192.168.199.204:8080/"    //  本地测试环境
-let BaseUrl: String = "http://localhost:8080/"    //  本地测试环境
+let BaseUrl: String = "http://localhost:8081/"    //  本地测试环境
 
 export default BaseUrl
